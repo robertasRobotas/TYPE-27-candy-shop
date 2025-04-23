c60427d6e5165bd7ee38aa8e0e84ae40500eb9b0
Robertas Ankudovicius <r.ankudovicius@gmail.com>
2025-04-23 09:20:01 +0300

    Added landing page

---
+++ insert/script.js
@@ -0,0 +1,37 @@
+// const candiesWrapper = document.getElementById("candies-wrapper");
+
+// const fetchCandies = async () => {
+//   const response = await fetch(
+//     "https://680726a0e81df7060eb8fba8.mockapi.io/candies"
+//   );
+
+//   const data = await response.json();
+//   return data;
+// };
+
+// const buildCards = (data) => {
+//   data.forEach((d) => {
+//     const card = document.createElement("div");
+//     card.classList.add("card");
+
+//     const title = document.createElement("h2");
+//     title.textContent = d.title;
+
+//     const img = document.createElement("img");
+//     img.src = d.imgUrl;
+
+//     card.append(img);
+//     card.append(title);
+
+//     console.log(title);
+
+//     candiesWrapper.append(card);
+//   });
+// };
+
+// const buildScreen = async () => {
+//   const candies = await fetchCandies();
+//   buildCards(candies);
+// };
+
+// buildScreen();
